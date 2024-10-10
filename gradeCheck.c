4a8bd6fe89ab5d236dfdb92ce5ff0c76d57549a6
glennnoronha <glenn.noronha@yahoo.com>
2024-10-10 15:24:25 -0500

    Working with Switches

---
+++ gradeCheck.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+
+int main() {
+
+    char grade;
+
+    printf("Enter your grade: ");
+    scanf(" %c", &grade);
+
+    switch (grade)
+    {
+    case 'A':
+        printf("Excellent!\n");
+        break;
+
+    case 'B':
+        printf("Well done!\n");
+        break;
+    case 'C':
+        printf("Good job!\n");
+        break;
+    
+    default:
+        printf("Keep trying!\n");
+        break;
+    }
+    return 0;
+}
