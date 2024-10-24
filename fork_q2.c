481c4c765aae361b6fb2e82dd4d435475a16086e
glennnoronha <glenn.noronha@yahoo.com>
2024-10-23 20:17:53 -0500

    update

--- fork_q2.c
+++ fork_q2.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int main() {
-    FILE* fp = fopen("text.txt", "a");
-
-    if (fp == NULL) {
-        printf("Error opening file\n");
-        return 1;
+    int fd = open("output.txt",  | O_CREAT | O_TRUNC);
+    if (fd < 0) {
+        perror("Failed to open the file");
+        exit(1);
     }
 
     pid_t pid = fork();
 
     if (pid < 0) {
-        printf("Fork failed!\n");
-        return 1;
+        perror("fork() failed");
+        exit(1);
     } else if (pid == 0) {
-        fprintf(fp, "Child writing\n");
+        printf("Child process writing...\n");
+        for (int i = 0; i < 5; i++) {
+            if (write(fd, "Child writing...\n", 17) != 17) {
+                perror("Write error in child process");
+            }
+        }
     } else {
-        fprintf(fp, "Parent writing\n");
+        printf("Parent process writing...\n");
+        for (int i = 0; i < 5; i++) {
+            if (write(fd, "Parent writing...\n", 18) != 18) {
+                perror("Write error in parent process");
+            }
+        }
     }
 
-    fclose(fp);
+    close(fd);
     return 0;
 }
